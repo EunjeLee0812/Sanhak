8ecfa975f726f2b0e38f9fdaeebcae0e59bf2cf9
서원렬 <dnjsfuf793@skku.edu>
2026-02-09 12:23:57 +0000

    - biasmgr.finalize의 인자 bias_weight_update_cnt에서 의미에 맞게 repeat로 변경 - output csv summary에 file_cnt열 추가 - output csv detail의 hard_missed_pn 열 이름 soft_missed_pn로 변경

--- config/settings.py
+++ config/settings.py
@@ -21,12 +21,12 @@ BIAS_PATH = "/teamspace/studios/this_studio/Sanhak/lists/biasing_list.json"
 # ==============================================================================
 # 2. 실험 변수 (Hyper-parameters)
 # ==============================================================================
-HOTWORD_TOPK_SWEEP = [20,30] #20고정
-BIAS_WEIGHT_UPDATE_ITERATION_SWEEP = [1,2]  # 전체 학습 반복 횟수
+HOTWORD_TOPK_SWEEP = [20] #20고정
+BIAS_WEIGHT_UPDATE_ITERATION_SWEEP = [10, 20]  # 전체 학습 반복 횟수
 POSTPROCESS_SWEEP = [1] # 0: OFF, 1: ON
 HOTWORD_STRATEGY_SWEEP = [2] # 1: Random, 2: Hybrid 2로 고정
 RESET_BIASING_LIST=1 #BIAS_ITERATION_SWEEP[index]만큼의 학습 회차를 돈 후 다음 biasing_list를 초기화할지 [0:OFF, 1:ON]
-AUDIO_FILE_MAX= 2    # 사용할 최대 AUDIO FILE의 개수
+AUDIO_FILE_MAX= 20    # 사용할 최대 AUDIO FILE의 개수
 
 # ==============================================================================
 # 3. 모델 및 로직 설정
@@ -45,5 +45,5 @@ RULE_TOL = 2
 
 #가중치 업데이트 관련 파라미터 추가
 PN_MATCH_TH   = float(os.environ.get("PN_MATCH_TH", "0.20"))   # pn_recall 계산용
-HARD_MISS_TH  = float(os.environ.get("HARD_MISS_TH", "0.50"))  # 학습용 hard miss 기준
-SOFT_MISS_TH = float(os.environ.get("HARD_MISS_TH", "0.50"))  # 학습용 hard miss 기준
+# HARD_MISS_TH  = float(os.environ.get("HARD_MISS_TH", "0.50"))  # 학습용 hard miss 기준
+# SOFT_MISS_TH = float(os.environ.get("HARD_MISS_TH", "0.50"))  # 학습용 hard miss 기준
